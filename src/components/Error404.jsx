cf56267d7a8f8ac82eb2de2002d7f8bb66caf155
sanjeet0000 <sanjeetdeshwal@outlook.com>
2023-03-28 17:12:25 +0530

    fixed typo

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
-<div className="row flex-column justify-content-center align-items-center w-" style={{height:"350px"}}>
+<div className="row flex-column justify-content-center align-items-center text-center" style={{height:"350px"}}>
     <h1>Oops! You seem to be lost.</h1>
     <p>You can start from scratch:</p>
     <Link to='/'><h3>Home</h3></Link>
